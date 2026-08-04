68f58de7de44c81ee137f9cb7602e358a3c4a3eb
Bubu <bendeguzbubu@gmail.com>
2026-08-04 11:05:02 +0200

    Fix main initialization and map key - build 5

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Google Maps API kulcs regisztrálása (KÖTELEZŐ iOS-en az összeomlás ellen)
+    GMSServices.provideAPIKey("A_TE_GOOGLE_MAPS_API_KULCSOD")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hegygo/features/home/main_screen.dart'; // A projekt szerinti főképernyő importja
 
-void main() {
+void main() async {
+  // 1. KÖTELEZŐ: Megakadályozza a natív indítási összeomlást release módban
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. Globális hiba-elkapó (ha bármi hiba történne, nem engedi kilépni az appot)
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    debugPrint('HegyGO Kódhiba elkapva: ${details.exception}');
+  };
+
   runApp(const HegyGoApp());
 }
 
